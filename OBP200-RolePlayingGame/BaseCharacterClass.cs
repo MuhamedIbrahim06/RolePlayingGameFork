a9e51ee28d8dccecc2ba3734e18b65d0296f5e15
MuhamedIbrahim06 <Muhamed.ibra06@gmail.com>
2026-03-25 16:28:36 +0100

    Clean Code: Bytte ut magiska tal i BaseCharacterClass.cs till const int med tydliga namn

--- OBP200-RolePlayingGame/BaseCharacterClass.cs
+++ OBP200-RolePlayingGame/BaseCharacterClass.cs
@@ -11,9 +11,11 @@ public abstract class BaseCharacterClass : ICharacterClass
     public abstract int LevelUpHpBonus { get; }
     public abstract int LevelUpAtkBonus { get; }
     public abstract int LevelUpDefBonus { get; }
+    protected const int MinimumDamage = 1;
+    protected const int DefenseDivider = 2;
 
     protected int GetBaseDamage(int playerAtk, int enemyDef)
     {
-        return Math.Max(1, playerAtk - (enemyDef / 2));
+        return Math.Max(MinimumDamage, playerAtk - (enemyDef / DefenseDivider));
     }
 }
