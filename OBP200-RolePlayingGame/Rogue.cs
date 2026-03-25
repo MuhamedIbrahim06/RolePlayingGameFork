0da94cfde7c6cd78ca3b9e29eb5820e82c2a63e4
MuhamedIbrahim06 <Muhamed.ibra06@gmail.com>
2026-03-25 14:35:46 +0100

    Lade till attributer och metoder i klassen Rogue

--- OBP200-RolePlayingGame/Rogue.cs
+++ OBP200-RolePlayingGame/Rogue.cs
@@ -1,6 +1,21 @@
 ﻿namespace OBP200_RolePlayingGame;
 
-public class Rogue : ICharacterClass
+public class Rogue : BaseCharacterClass
 {
-    
+    public override string ClassName => "Rogue";
+    public override double FleeChance => 0.5;
+    public override int SpecialAttackGoldCost => 0;
+    public override int SpecialAttackHealthCost => 0;
+    private static readonly Random _Rng = new Random();
+
+    public override int CalculateBaseDamage(int playerAtk, int enemyDef)
+    {
+        int critBonus = _Rng.NextDouble() < 0.2 ? 4 : 0;
+        return GetBaseDamage(playerAtk, enemyDef) + critBonus;
+    }
+
+    public override int CalculateSpecialAttack(int playerAtk, int enemyDef)
+    {
+        
+    }
 }
