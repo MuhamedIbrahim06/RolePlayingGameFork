3f76798de23b025a8f827f184c4966b936f47c97
MuhamedIbrahim06 <muhamed.ibra06@gmail.com>
2026-05-07 16:51:10 +0200

    Refaktorisering: Uppdaterade mage, uppdaterade specialattack samt säkerställer att den skriver ut samma som i originalkoden

--- OBP200-RolePlayingGame/Mage.cs
+++ OBP200-RolePlayingGame/Mage.cs
@@ -4,8 +4,6 @@ public class Mage : BaseCharacterClass
 {
     public override string ClassName => "Mage";
     public override double FleeChance => 0.35;
-    public override int SpecialAttackGoldCost => 3;
-    public override int SpecialAttackHealthCost => 0;
     public override int LevelUpHpBonus => 4;
     public override int LevelUpAtkBonus => 4;
     public override int LevelUpDefBonus => 1;
@@ -15,8 +13,20 @@ public class Mage : BaseCharacterClass
         return GetBaseDamage(playerAtk, enemyDef) + 2;
     }
 
-    public override int CalculateSpecialAttack(int playerAtk, int enemyDef)
+    public override int PerformSpecialAttack(Player player, int enemyDef, bool vsBoss)
     {
-        return Math.Max(3, playerAtk + 5 - (enemyDef / 2));
+        if (player.Gold >= 3)
+        {
+            Console.WriteLine($"Mage kastar Fireball!");
+            player.SpendGold(3);
+            int damage = (int)Math.Max(3, player.Attack + 5 - (enemyDef / 2));
+            if (vsBoss) damage = (int)Math.Round(damage * 0.8);
+            return damage;
+        }
+        else
+        {
+            Console.WriteLine($"Inte tillräckligt med guld för att kasta Fireball (kostar 3).");
+            return 0;
+        }
     }
 }
